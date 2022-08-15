384f8b66f1e2f3d05d73e134308679151d68ba7f
3dzoneeee <110004451+3dzoneeee@users.noreply.github.com>
2022-08-15 13:38:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+ let img = document.querySelector('img');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+btn3.addEventListener('click', () => {
+    img.src = ' img/sad.png';
+})
+btn4.addEventListener('click', () => {
+    img.src = 'img/happy.png';
+})
+ 
+   
